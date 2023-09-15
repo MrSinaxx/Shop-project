45a7b1dc83cdb8265e3909f8cd2ee68b8e60e52e
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 16:32:03 +0330

    feat: AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.CustomUser"
 
+AUTHENTICATION_BACKENDS = ("django.contrib.auth.backends.ModelBackend",)
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
