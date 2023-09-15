d6e916e4e14e2dfe044116efe337b112022626f2
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 15:41:59 +0330

    feat: OrderItem model

--- store/models.py
+++ store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from users.models import Customer
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -15,3 +18,10 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
