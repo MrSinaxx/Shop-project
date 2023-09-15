6d496366e9bc89e51c476c43c78c6c46131c71ad
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 15:38:34 +0330

    feat: Customer model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
