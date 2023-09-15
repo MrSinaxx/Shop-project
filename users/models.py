deb1090b41ada8e59c4f1bbb5a006ec63870d652
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 15:39:12 +0330

    feat: Address model

--- users/models.py
+++ users/models.py
@@ -14,3 +14,11 @@ class Employee(models.Model):
     email = models.EmailField()
     job_title = models.CharField(max_length=255)
     hire_date = models.DateTimeField()
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
