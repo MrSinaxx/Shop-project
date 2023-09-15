860f382b48ac36cf59ba94a0fedf155b366b07b9
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 15:38:50 +0330

    feat: Employee model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,11 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    job_title = models.CharField(max_length=255)
+    hire_date = models.DateTimeField()
