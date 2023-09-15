8480e9d3c6b20045230621e3c5eaf6dba39b1c49
MrSinxx <sinaleylaz@yahoo.com>
2023-09-15 16:35:20 +0330

    feat: CustomUserSerializer & OTPSerializer

---
+++ users/serializers.py
@@ -0,0 +1,24 @@
+# users/serializers.py
+from rest_framework import serializers
+from .models import CustomUser
+
+
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "phone_number",
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_staff",
+            "date_joined",
+        ]
+
+
+class OTPSerializer(serializers.Serializer):
+    otp = serializers.IntegerField()
